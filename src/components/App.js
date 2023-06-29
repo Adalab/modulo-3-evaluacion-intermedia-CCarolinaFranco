e3ddca4786d38637553f1ec990ad278e860b5ab0
CCarolinaFranco <cirilina@hotmail.com>
2023-06-29 16:31:21 +0100

    changed  build for docs

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,82 @@
-//import '../styles/App.scss'
-//import data from '../data/data.json'
+
+
 import { useEffect, useState } from 'react';
 
+function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [filterText, setFilterText] = useState('');
+  const [filterCharacter, setFilterCharacter] = useState('all');
 
+  useEffect(() => {
+    fetch('https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json')
+      .then((response) => response.json())
+      .then((data) => setQuotes(data));
 
+  }, []);
 
-  function App() {
-    const [quotes, setQuotes] = useState([]);
-    const [filterText, setFilterText] = useState('');
-    const [filterCharacter, setFilterCharacter] = useState('');
-    
-    useEffect (() => {
-      fetch('https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json')
-        .then((response) => response.json())
-        .then((data) => setQuotes(data));
-  
-      }, []);
+  const handleInputFilter = (ev) => {
+    setFilterText(ev.target.value);
 
-      const handleInputFilter = (ev) =>{
-        setFilterText(ev.target.value);
+  }
 
-      }
-
-      const handleFilterCharacter = (event) => {
-        setFilterCharacter(event.target.value);
-      }; 
-
-    {/*const filteredQuotes = quotes.filter((quote) => {
-    const quoteText = quote.quote.toLowerCase();
-    const searchText = filterText.toLowerCase();
-    const characterName = quote.character.toLowerCase();
-    const searchCharacter = filterCharacter.toLowerCase();*/}
-
-    return quoteText.includes(searchText) && characterName.includes(searchCharacter);
-  };
-
-
-      const renderQuotes = () => {
-        return (
-          
-         quotes
-         //.filter
-         .map((eachQuote, index) => (
-        <li key={index}>
-          {eachQuote.character}: {eachQuote.quote}
-        </li>
-        )));
-    
-      };
-        
+  const handleFilterCharacter = (event) => {
+    setFilterCharacter(event.target.value);
+  }
+  const renderQuotes = () => {
     return (
-      <div className="container">
-      <h1>Frases de Friends</h1>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Filtrar por frase"
-          value={filterText}
+      quotes
+        .filter((cadaQuote) => cadaQuote.quote.includes(filterText.toLowerCase()))
+        
+
+
+
+        .map((eachQuote, index) => (
+          <li key={index}>
+            {eachQuote.character}: {eachQuote.quote}
+          </li>
+        )));
+
+  }
+
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Frases de Friends</h1>
+      </header>
+
+      <form>
+        <label>Filtrar por frase</label>
+        <input type="text" placeholder="Filtrar por frase" value={filterText}
           onChange={handleInputFilter}
         />
-      </div>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Filtrar por personaje"
+        <label>Personajes</label>
+        <select name="" id=""
           value={filterCharacter}
-    onChange={handleFilterCharacter}
-        />
+          onChange={handleFilterCharacter}>
+          
+          <option value= "all">Todos</option>
+          <option value= "Phoebe">Phoebe</option>
+          <option value= "Rachel">Rachel</option>
+          <option value= "Rachel">Ross</option>
+          <option value= "Monic">Monica</option>
+          <option value= "Chandler">Chandler</option>
+          <option value= "Joey">Joey</option>
+
+        </select>
+
+      </form>
+
+      <ul>{renderQuotes()}</ul>
     </div>
-
-      <ul>{renderQuotes ()}</ul>
-    </div>
-      );
-
-    }
+  );
 
 
-    
 
 
+}
 
 export default App;
 
